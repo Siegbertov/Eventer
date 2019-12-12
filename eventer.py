add9698b49cbd5ad647a9ff1a31d46c69d53e91d
Vladyslav Ziberov <siegbertov@gmail.com>
2019-12-12 10:08:13 +0200

    Created Main Window

---
+++ eventer.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+from datetime import datetime
+import time
+import sqlite3
+
+
+root = Tk()
+root.geometry("{}x{}".format(500, 500))
+root.title("Eventer")
+
+root.mainloop()
